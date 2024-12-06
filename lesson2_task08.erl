d1e618574d98cc97b5c04d177a52189c8d6af115
RVovchanskyi01 <mag5663065@stud.duikt.edu.ua>
2024-12-06 12:55:44 +0200

    пакування послідовних дублікатів у вкладені списки

--- lesson2_task08.erl
+++ lesson2_task08.erl
@@ -5,10 +5,10 @@ compress(List) ->
     compress(List, []).
 
 compress([], Acc) ->
-    lists:reverse(Acc); % Reverse the accumulator to get the correct order
+    lists:reverse(Acc); 
 compress([H|T], []) ->
-    compress(T, [H]); % Initialize the accumulator with the first element
+    compress(T, [H]); 
 compress([H|T], [H1|_]=Acc) when H =:= H1 ->
-    compress(T, Acc); % Skip the element if it's the same as the last one in the accumulator
+    compress(T, Acc); 
 compress([H|T], Acc) ->
-    compress(T, [H|Acc]). % Add the element to the accumulator if it's different
+    compress(T, [H|Acc]). 
