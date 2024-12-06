11717d5c1c21718013bb69c1b46fd8ce25a2a505
RVovchanskyi01 <mag5663065@stud.duikt.edu.ua>
2024-12-06 11:59:35 +0200

    Реалізовано функцію для знаходження N-го елемента списку без оператора ++ та OTP

---
+++ lesson2_task03.erl
@@ -0,0 +1,5 @@
+-module(lesson2_task03).
+-export([element_at/2]).
+element_at([X | _], 1) -> X;  
+element_at([_ | Tail], N) when N > 1 -> element_at(Tail, N - 1); 
+element_at([], _) -> undefined.  
